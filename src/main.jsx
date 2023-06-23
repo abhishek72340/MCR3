20c8114f1a3482abc21e4b4666b5c53a2bd0fb3b
abhishek72340 <abhisingh.72340@gmail.com>
2023-06-23 21:25:58 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { MCRProvider } from './Context/context.jsx'
-import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
       <MCRProvider>
         <App />
       </MCRProvider>
-    </BrowserRouter>
+   
   </React.StrictMode>,
 )
